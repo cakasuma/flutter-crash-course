ea7d0d898d125232f1e7c2a187a1bd48b4e2a5c9
Amam Mustofa <mustofa@binary.com>
2018-12-09 21:34:04 +0800

    hellow world

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter crash 1',
+      home: MyHomePage(title: 'Welcome'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: navigationBar(context),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello world',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget navigationBar(BuildContext context) {
+    return AppBar(
+      title: Text(widget.title)
+    );
+  }
+}
